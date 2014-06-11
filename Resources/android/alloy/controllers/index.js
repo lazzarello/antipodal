6ffa235479b7372ae663459baf20eb61107aeb87
Lee Azzarello <lee@seriesdigital.com>
2014-06-11 14:49:38 -0400

    Add default native code and resources from Titanium

---
+++ Resources/android/alloy/controllers/index.js
@@ -0,0 +1,28 @@
+function Controller() {
+    require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
+    this.__controllerPath = "index";
+    arguments[0] ? arguments[0]["__parentSymbol"] : null;
+    arguments[0] ? arguments[0]["$model"] : null;
+    arguments[0] ? arguments[0]["__itemTemplate"] : null;
+    var $ = this;
+    var exports = {};
+    $.__views.index = Ti.UI.createWindow({
+        backgroundColor: "white",
+        id: "index"
+    });
+    $.__views.index && $.addTopLevelView($.__views.index);
+    $.__views.map = require("ti.map").createView({
+        id: "map",
+        userLocation: "true",
+        region: "{latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 }"
+    });
+    $.__views.index.add($.__views.map);
+    exports.destroy = function() {};
+    _.extend($, $.__views);
+    $.index.open();
+    _.extend($, exports);
+}
+
+var Alloy = require("alloy"), Backbone = Alloy.Backbone, _ = Alloy._;
+
+module.exports = Controller;
