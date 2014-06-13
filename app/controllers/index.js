a4a8d1798ced44df6e4bdc5871bff128e56daf81
Lee Azzarello <lee@seriesdigital.com>
2014-06-13 13:45:06 -0400

    call some geolocation stuff and functions!

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,14 +1,45 @@
-function doClick(e) {
-    alert($.label.text);
+function report(evt) {
+	Ti.API.info("Annotation " + evt.title + " clicked, id: " + evt.annotation.myid);
 }
 
-var lon = 1;
-if ( lon < 0 ) {
-  longe = lon + 180;
-} else if (lon > 0) {
-  lon = lon - 180;
-} else {
-	print("you're on the equator");
-}
+/*
+$.mapview.region = {latitude:0, longitude:0,
+					latitudeDelta:0.01,longitudeDelta:0.01 };
+*/
+
+$.mapview.userLocation = true;
+$.mapview.regionFit = true;
+$.mapview.regionFit = Alloy.Globals.Map.NORMAL_TYPE;
+
+var GeoData = function(title,lat,lon) {
+	if ( lon < 0 ) {
+  		lon = lon + 180;
+	} else if (lon > 0) {
+  		lon = lon - 180;
+	} else {
+		print("you're on the equator");
+	}
+	this.title = title;
+	this.coords = {
+		latitude: lat,
+		longitude: lon
+	};
+};
+
+
+var latitude;
+var longitude;
+
+var callback = function(e) {
+	if (e.error) {
+		alert("cannot get your location");
+		return;
+	} else {
+		longitude = e.coords.longitude;
+		latitude = e.coords.latitude;
+	};
+};
+
+Titanium.Geolocation.getCurrentPosition(callback);
 
 $.index.open();
