75f1a6ec4c0f8e31af2b00b79bd6ae68e201466d
Lee Azzarello <lee@seriesdigital.com>
2014-06-13 13:45:12 -0400

    build artifacts

--- Resources/iphone/alloy/controllers/index.js
+++ Resources/iphone/alloy/controllers/index.js
@@ -1,4 +1,7 @@
 function Controller() {
+    function report(evt) {
+        Ti.API.info("Annotation " + evt.title + " clicked, id: " + evt.annotation.myid);
+    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "index";
     arguments[0] ? arguments[0]["__parentSymbol"] : null;
@@ -6,20 +9,36 @@ function Controller() {
     arguments[0] ? arguments[0]["__itemTemplate"] : null;
     var $ = this;
     var exports = {};
+    var __defers = {};
     $.__views.index = Ti.UI.createWindow({
         backgroundColor: "white",
         id: "index"
     });
     $.__views.index && $.addTopLevelView($.__views.index);
-    $.__views.map = require("ti.map").createView({
-        id: "map",
-        userLocation: "true",
-        region: "{latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 }"
+    $.__views.mapview = Alloy.Globals.Map.createView({
+        id: "mapview",
+        ns: "Alloy.Globals.Map"
     });
-    $.__views.index.add($.__views.map);
+    $.__views.index.add($.__views.mapview);
+    report ? $.__views.mapview.addEventListener("click", report) : __defers["$.__views.mapview!click!report"] = true;
     exports.destroy = function() {};
     _.extend($, $.__views);
+    $.mapview.userLocation = true;
+    $.mapview.regionFit = true;
+    $.mapview.regionFit = Alloy.Globals.Map.NORMAL_TYPE;
+    var latitude;
+    var longitude;
+    var callback = function(e) {
+        if (e.error) {
+            alert("cannot get your location");
+            return;
+        }
+        longitude = e.coords.longitude;
+        latitude = e.coords.latitude;
+    };
+    Titanium.Geolocation.getCurrentPosition(callback);
     $.index.open();
+    __defers["$.__views.mapview!click!report"] && $.__views.mapview.addEventListener("click", report);
     _.extend($, exports);
 }
 
--- Resources/iphone/app.js
+++ Resources/iphone/app.js
@@ -1,6 +1,6 @@
 var Alloy = require("alloy"), _ = Alloy._, Backbone = Alloy.Backbone;
 
-var MapModule = require("ti.map");
+Alloy.Globals.Map = require("ti.map");
 
 var rc;
 
