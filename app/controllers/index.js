3422741670c1c529c62bbf73f3501bfe930946e4
Lee Azzarello <lee@seriesdigital.com>
2014-06-13 12:59:52 -0400

    stub out the longitude translation conditions

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,4 +2,13 @@ function doClick(e) {
     alert($.label.text);
 }
 
+var lon = 1;
+if ( lon < 0 ) {
+  longe = lon + 180;
+} else if (lon > 0) {
+  lon = lon - 180;
+} else {
+	print("you're on the equator");
+}
+
 $.index.open();
